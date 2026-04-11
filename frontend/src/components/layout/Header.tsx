132c74485b26ca012e286b1f3178e45baacc16d9
emanuel-malungo <emanuelmalungo856@gmail.com>
2026-04-11 17:05:45 +0100

    style: add object-center class to logo image for better alignment

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
             alt="Kubata"
             width={120}
             height={40}
-            className="h-10 w-auto"
+            className="h-10 w-auto  object-center"
           />
         </div>
 
